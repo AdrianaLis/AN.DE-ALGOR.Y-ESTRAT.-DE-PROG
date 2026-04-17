04f2b062229efe4522f04eae8f8a17f285e06906
AdrianaLis <urbinalis2003@gmail.com>
2026-04-16 22:39:12 -0500

    ejercicios

--- Clases.py
+++ Clases.py
@@ -17,4 +17,49 @@ q =Perro("Firu")
 print(q)
 q.ladrar()
 
+class Persona:  
+    def __init__(self):
+        self.nombre_usuario= input ("Ingrese nombre\n")
+        self.edad_usuario=int(input("Ingrese edad\n"))
+
+           
+    def mostrar(self):
+       print(f" Mi nombre es :{self.nombre_usuario} y tengo :{self.edad_usuario} años")
+    
+def menu():
+       usuario= None
+       while True:
+          print( "-----Menú------")
+          print("1.Ingresar datos")
+          print("2.Ver datos")
+          print ("3.Salir")
+          opcion= input(" Seleccione su opcion:")
+          if opcion=="1":
+             usuario = Persona()
+             print("Persona registrada con exito")
+          elif opcion=="2":
+             if usuario:
+                usuario.mostrar()
+             else: print("Errooor  Debe ingresar sus datos")
+          elif opcion=="3":
+             print("Saliendo del programa")
+             break
+          else: print("Opcion no valida")
+
+   
+menu()
+
+             
+
+       
+
+
+
+
+
+
+
+
+
+
 
